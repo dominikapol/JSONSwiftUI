282940701bd8ddc3fc76c69fd1f447c3ba92bad6
Dominika <dominikapiganova@mail.ru>
2022-02-23 11:09:18 +0200

    Initial Commit

---
+++ JSONSwiftUI/JSONSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONSwiftUIApp.swift
+//  JSONSwiftUI
+//
+//  Created by Dominika Poleshyck on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct JSONSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
